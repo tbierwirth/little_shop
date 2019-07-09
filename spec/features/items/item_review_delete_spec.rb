b44cbfcb2e0e8d6592f0b3c79f42c3c2a0c467af
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-07-09 15:42:52 -0600

    Adds testing for dependent destory of reviews on Item

---
+++ spec/features/items/item_review_delete_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Item Show Page' do
+  describe 'As a visitor' do
+    before :each do
+      @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @review1 = @giant.reviews.create!(title: "Damn!", rating: 5, body: "Definitely a Giant. No argument there")
+      @review2 = @giant.reviews.create!(title: "Disappointed...", rating: 1, body: "Idk what I was expecting but I was disappointed.")
+      @review3 = @giant.reviews.create!(title: "Fair.", rating: 3, body: "Not thrilled or upset. Was as expected for price.")
+      @review4 = @giant.reviews.create!(title: "Pretty Good.", rating: 4, body: "Good Giant, wish it has come in the mail a bit earlier. It was so hungry when I opened it.")
+      end
+
+    it "deleted Items will delete associated reviews" do
+      visit "/items/#{@giant.id}"
+
+      click_on "Delete"
+
+      expect(current_path).to eq("/items")
+
+    end
+
+
+  end
+end
+
+
+# As a visitor
+# When I delete an item
+# All reviews associated with that item are also deleted
--- spec/features/merchant/flash_message_spec.rb
+++ spec/features/merchant/flash_message_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+ require 'rails_helper'
 
 RSpec.describe 'Existing Merchant Update Flash Message' do
   describe 'As a Visitor' do
