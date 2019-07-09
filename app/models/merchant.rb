1805dc058964abc36c4c8f68b85a11323db976ab
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-09 14:21:25 -0600

    Add boolean method for merchant item orders

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,11 @@
 class Merchant < ApplicationRecord
-  has_many :items, :dependent => :restrict_with_exception
+  has_many :items, dependent: :destroy
+
+  def self.can_destroy?(merchant)
+    answer = ItemOrder.all.map do |item|
+      Item.find(item.item_id).merchant_id == merchant.id
+    end
+    answer == (false || [])
+  end
+
 end
