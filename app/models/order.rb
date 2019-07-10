60d3289a351dff4a81c0d5fb3602a3578ed2a1a8
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-10 17:14:33 -0600

    Add verification_code method

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,10 @@
 class Order < ApplicationRecord
-validates_presence_of :name, :address, :city, :state, :zip
+  validates_presence_of :name, :address, :city, :state, :zip
 
-has_many :item_orders
-has_many :items, through: :item_orders
+  has_many :item_orders
+  has_many :items, through: :item_orders
+
+  def verification_code
+    10.times.map{rand(10)}.join
+  end
 end
