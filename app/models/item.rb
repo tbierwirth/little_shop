9d6ea11abba8ac9cadf2c90fc8d65a9e5746cc2a
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-07-10 11:22:16 -0600

    Adds Model Tests for merchant.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :item_orders
   has_many :orders, through: :item_orders
-  
+
   validates_presence_of :name
   validates_presence_of :description
   validates_presence_of :price
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -15,6 +15,40 @@ RSpec.describe Merchant do
       expect(Merchant.can_destroy?(@brian)).to be false
     end
 
+    it "#count_items" do
+      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @ogre = @brian.items.create!(name: 'Ogre', description: "I'm a Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+      @unicorn= @brian.items.create!(name: 'Unicorn', description: "I'm a Unicorn!", price: 200, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 1 )
+      @order = Order.create!(name: "Bill", address: "123", city: "Denver", state: "CO", zip: "80210")
+      expect(@brian.count_items).to eq(3)
+    end
+
+    it "#avg_price_items" do
+      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @ogre = @brian.items.create!(name: 'Ogre', description: "I'm a Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+      @unicorn= @brian.items.create!(name: 'Unicorn', description: "I'm a Unicorn!", price: 200, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 1 )
+      @order = Order.create!(name: "Bill", address: "123", city: "Denver", state: "CO", zip: "80210")
+
+      expect(@brian.avg_price_items).to eq(90)
+    end
+
+    it "#cities_ordered" do
+      @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @order1 = Order.create!(name: 'PaulieP', address: 'yo yo st', city: 'Denver', state: 'CO', zip: 80218)
+      @order2 = Order.create!(name: 'Kanye', address: 'Lime Lite st', city: 'Chicago', state: 'IL', zip: 90210)
+      @order3 = Order.create!(name: 'Me', address: 'yo yo st', city: 'Denver', state: 'CO', zip: 90210)
+      @item_order1 = @giant.item_orders.create!(order_id: @order1.id, item_id: @giant.id, quantity: 2, price: @giant.price)
+      @item_order2 = @ogre.item_orders.create!(order_id: @order2.id, item_id: @ogre.id, quantity: 3, price: @ogre.price)
+      @item_order3 = @ogre.item_orders.create!(order_id: @order3.id, item_id: @ogre.id, quantity: 4, price: @ogre.price)
+
+      expect(@megan.cities_ordered).to eq("Chicago, Denver")
+    end
+
   end
 
 end
