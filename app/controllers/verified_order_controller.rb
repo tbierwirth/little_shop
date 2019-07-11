43d399c5d006993606045b9b896ba338acc5aa56
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-10 22:52:25 -0600

    Add destroy order action

--- app/controllers/verified_order_controller.rb
+++ app/controllers/verified_order_controller.rb
@@ -4,6 +4,12 @@ class VerifiedOrderController < ApplicationController
     @order = Order.where(verification_code: code["Search"]).first
   end
 
+  def destroy
+    Order.destroy(params[:verified_order_id])
+    flash[:notice] = "Your order has been deleted."
+    redirect_to '/'
+  end
+
 private
   def code
     params.permit("Search")
