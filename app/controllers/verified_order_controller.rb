55d1a9f6ed288ca0d8272ac662b504aaf1205d8a
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-10 23:43:13 -0600

    Add actions to update and edit

--- app/controllers/verified_order_controller.rb
+++ app/controllers/verified_order_controller.rb
@@ -2,6 +2,17 @@ class VerifiedOrderController < ApplicationController
 
   def show
     @order = Order.where(verification_code: code["Search"]).first
+
+  end
+
+  def edit
+    @order = Order.find(params[:verified_order_id])
+  end
+
+  def update
+    @order = Order.find(params[:verified_order_id])
+    @order.update_attributes(merchant_params)
+    render :show
   end
 
   def destroy
@@ -14,6 +25,10 @@ class VerifiedOrderController < ApplicationController
 private
   def code
     params.permit("Search")
+  end
+
+  def merchant_params
+    params.permit(:name, :address, :city, :state, :zip)
   end
 
 end
